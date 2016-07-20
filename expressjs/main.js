1ad2d139adca533ce869d26df20f058440405544
shawn <shawn_en@yeah.net>
2016-07-20 15:54:30 +0800

    blog

--- expressjs/main.js
+++
@@ -1,8 +0,0 @@
-var newTemps = document.getElementById("newTemps");
-var tmpl1 = "<p>val is : <%= vals %></p>";
-
-setInterval(function(){
-	var val = document.getElementById("new-tem-val").value || null;
-	console.log(val)
-	newTemps.innerHTML = ejs.render(tmpl1,{vals:val});
-},2000)
